8a55ed3b8efa06259223d29ef41f2051912e307d
mahimfarhad <mahimfarhad.code@gmai.com>
2024-04-24 23:17:11 +0600

    Update Sidebar Context

--- components/navigations/TopNav.jsx
+++ components/navigations/TopNav.jsx
@@ -94,7 +94,7 @@ function TopNav() {
                       "overflow-hidden"
                     )
 
-                    setSidebarSlide((prev) => !prev)
+                    setSidebarSlide(true)
                   }
                 }}
               />
